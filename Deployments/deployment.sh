e9fa256535aec0ff8f84ee6623a435f247859781
Glenn Benz <glenn.benz@gmail.com>
2025-11-21 17:45:33 -0500

    Add a deployment script

---
+++ Deployments/deployment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+
+# Create a deployment named nginx using the nginx:latest image
+kubectl create deployment nginx --image=nginx:latest
+kubectl get deployments
+kubectl describe deployment nginx
+kubectl get pods -o wide -l app=nginx
+
+# Scale the deployment to 3 replicas
+kubectl scale deployment nginx --replicas=3
+kubectl get pods -o wide -l app=nginx
+
+# Label all nginx pods with tier=frontend
+for i in $(kubectl get pods -l app=nginx -o jsonpath='{.items[*].metadata.name}'); do
+    kubectl label pod $i tier=frontend
+done
+kubectl get pods --show-labels
+kubectl get pods -l tier=frontend
+
+# Delete the deployment
+kubectl delete deployment nginx
+kubectl get deployments
