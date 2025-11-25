f1db67f14cd502a9c249a575ba1a01184068d876
Glenn Benz <glenn.benz@gmail.com>
2025-11-25 15:23:52 -0500

    Change volume binding to immediate

--- StatefulSets/StatefulSet.sh
+++ StatefulSets/StatefulSet.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 set -x
 
+# Create StorageClass and PVC
+kubectl apply -f StorageClass+PVC.yaml
+
+# Create 5 PVs
+for i in 0 1 2 3 4; do
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: data-redis-$i
+spec:
+  capacity:
+    storage: 20Mi
+  accessModes:
+    - ReadWriteOnce
+  hostPath:
+    path: /data/data-redis-$i
+EOF
+done
+
 kubectl apply -f StatefulSetService.yaml
 kubectl apply -f StatefulSet.yaml
 kubectl get pods -l app=redis
