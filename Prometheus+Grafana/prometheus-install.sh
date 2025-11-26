86eaf7cc014c77bf6bbcf632d97083d6958882c1
Glenn Benz <glenn.benz@gmail.com>
2025-11-26 07:58:35 -0500

    prometheus install script and pv allocation

---
+++ Prometheus+Grafana/prometheus-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+# Add helm repository
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+# Install Prometheus
+helm install prometheus prometheus-community/prometheus
+# Verify installation
+kubectl get pods -l app.kubernetes.io/name=prometheus
+# Port forward to access Prometheus UI
+kubectl port-forward svc/prometheus-server 9090:80
+# Access Prometheus at http://localhost:9090
