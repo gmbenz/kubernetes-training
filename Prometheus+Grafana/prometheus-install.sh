bb6b7a2498a54d3a24aadd8938e36689719b6ce8
Glenn Benz <glenn.benz@gmail.com>
2025-11-26 09:07:03 -0500

    Create a pv for the persistent volume controller

--- Prometheus+Grafana/prometheus-install.sh
+++ Prometheus+Grafana/prometheus-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 
+# Create a PV for Prometheus
+kubectl apply -f prometheus-pvs.yaml
 # Add helm repository
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 # Install Prometheus
 helm install prometheus prometheus-community/prometheus
-# Create a PV for Prometheus
-kubectl apply -f prometheus-pv.yaml
 # Verify installation
 kubectl get pods -l app.kubernetes.io/name=prometheus
 # Port forward to access Prometheus UI
